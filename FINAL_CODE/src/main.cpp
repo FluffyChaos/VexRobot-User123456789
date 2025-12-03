dc3da3ec55b5fff89fb305311d991fa613a750fc
Batfede <federico.battistoni@icloud.com>
2025-12-03 09:09:44 -0800

    Update main.cpp

--- FINAL_CODE/src/main.cpp
+++ FINAL_CODE/src/main.cpp
@@ -18,6 +18,8 @@
 // Gyroscope            inertial      5               
 // RingMotor            motor         6               
 // LineTracker          line          H               
+// Distance_x           distance      10              
+// Distance_y           distance      7               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -34,28 +36,35 @@ competition Competition;
 
 double leftControl = 0, rightControl = 0, leftPower = 0, rightPower = 0, spinControl = 0;
 double x = 0, y = 0;
+double delta = 0;
 double ringControl = 0;
+double rotation_ratio = 1720;
 #define PI 3.14159265358979323846
 double maxSpeed = 100;
-RingMotor.resetPosition();
 bool toggle = false;
 
 // start added variables
-double distance_x = 14.4; // re measure
-double distance_y = 13.8; // re measure
-// ps in competition we should measure these distances again since the playing field might be different
-double angle = 0; // deg
+double distance_x = 14.8;
+double distance_y = 14.57;
+double angle_end = 0; // deg
 double delta_angle = 0;
+double delta_x = 0;
+double delta_y = 0;
 bool allignement_toggle = false;
 bool angle_toggle = false;
-double k = 1/3; // scaling factor
+double k = 1.5; // scaling factor
 // end added variables
+
 
 void pre_auton(void) {
 
 }
 
 void autonomous(void) {
+
+  // Idea for new autonomous
+
+  // end Idea
   
   while (LineTracker.reflectivity() < 10) {
     leftControl = 0;
@@ -83,7 +92,7 @@ void usercontrol(void) {
     task::sleep(500);
   }
   double original_heading = Gyroscope.heading(degrees);
-
+  RingMotor.resetPosition();
   while(true) {
 
     if (Controller1.ButtonA.pressing()) {
@@ -116,37 +125,31 @@ void usercontrol(void) {
       toggle = true;
     }
     else if (Controller1.ButtonL1.pressing()) {
-      toggle = false;
-      RingMotor.spin(forward, maxSpeed, percent);
-      wait(1, seconds);
+      //toggle = false;
+      RingMotor.spin(reverse, maxSpeed, percent);
+      wait(0.6, seconds);
     }
       // bringing ringmotor into dock
-    else if (toggle && RingMotor.position() % 3000 != 0){
-      RingMotor.spin(forward, RingMotor.position() % 3000 + 10 * (RingMotor.position()/abs(RingMotor.position()), percent);
+    else if (toggle && fabs(remainder(RingMotor.position(degrees), rotation_ratio)) != 0){
+      RingMotor.spin(forward, -remainder(RingMotor.position(degrees), rotation_ratio)/100 + 10 * int((-RingMotor.position(degrees)/abs(RingMotor.position(degrees)))), percent);
     }
     else  {
       RingMotor.stop();
     }
 
-    // start added part
-    // using two distance sensors we can let the robot autonoumsly find the correct position to dunk
-    if (Controller1.ButtonX.pressing()) {
-      allignement_toggle = true;
-      angle_toggle = true;
-    }
-
     // fixing angle
-    if (angle_toggle) {
-      delta_angle = Gyroscope.rotation(degrees) - angle;
+    if (Controller1.ButtonX.pressing()) {
+      RingMotor.stop();
+      delta_angle = Gyroscope.rotation(degrees) - angle_end;
       if (delta_angle > 180) delta_angle -= 360;
       if (delta_angle < -180) delta_angle += 360;
-      while (delta_angle >= 1) {
-        NorthMotor.spin(forward, -delta_angle - 10 * ((delta_angle)/(abs(delta_angle))), percent);
-        SouthMotor.spin(forward, delta_angle + 10 * ((delta_angle)/(abs(delta_angle))), percent);
-        EastMotor.spin(forward, -delta_angle - 10 * ((delta_angle)/(abs(delta_angle))), percent);//is actually west on the robot
-        WestMotor.spin(forward, delta_angle + 10 * ((delta_angle)/(abs(delta_angle))), percent);
+      while (abs(delta_angle) >= 2) {
+        NorthMotor.spin(forward, delta_angle/2 + 10 * ((delta_angle)/(abs(delta_angle))), percent);
+        SouthMotor.spin(forward, -delta_angle/2 - 10 * ((delta_angle)/(abs(delta_angle))), percent);
+        EastMotor.spin(forward, -delta_angle/2 - 10 * ((delta_angle)/(abs(delta_angle))), percent);//is actually west on the robot
+        WestMotor.spin(forward, delta_angle/2 + 10 * ((delta_angle)/(abs(delta_angle))), percent);
         wait(20, msec);
-        delta_angle = Gyroscope.rotation(degrees) - angle;
+        delta_angle = Gyroscope.rotation(degrees) - angle_end;
         if (delta_angle > 180) delta_angle -= 360;
         if (delta_angle < -180) delta_angle += 360;
       }
@@ -156,23 +159,32 @@ void usercontrol(void) {
       WestMotor.stop();
 
       angle_toggle = false;
+      allignement_toggle = true;
       }
-
+      
     // fixing position
     while (allignement_toggle) {
       delta_x = Distance_x.objectDistance(inches) - distance_x;
       delta_y = Distance_y.objectDistance(inches) - distance_y;
+      double delta_variation = Gyroscope.rotation(degrees) - angle_end;
+      while (delta_variation > 180) delta_variation -= 360;
+      while (delta_variation < -180) delta_variation += 360;
+      //double c = cos(delta_variation);
+      //double s = sin(delta_variation);
+      x = -(delta_x)*k - 5 * ((delta_x)/(fabs(delta_x)));
+      y = -(delta_y)*k - 5 * ((delta_y)/(fabs(delta_y)));
+      double k_angle = 1;
       if (fabs(delta_x) >= 0.3) {
-        NorthMotor.spin(forward, -(delta_x)*k - 5 * ((delta_x)/(abs(delta_x))), percent);
-        SouthMotor.spin(forward, -(delta_x)*k - 5 * ((delta_x)/(abs(delta_x))), percent);
+        NorthMotor.spin(forward, x + delta_variation*k_angle, percent);
+        SouthMotor.spin(forward, x - delta_variation*k_angle, percent);
       }
       else {
         NorthMotor.stop();
         SouthMotor.stop();
       }
       if (fabs(delta_y) >= 0.3) {
-        EastMotor.spin(forward, -(delta_y)*k - 5 * ((delta_y)/(abs(delta_y))), percent);
-        WestMotor.spin(forward, -(delta_y)*k - 5 * ((delta_y)/(abs(delta_y))), percent);
+        EastMotor.spin(forward, y - delta_variation*k_angle, percent);
+        WestMotor.spin(forward, y + delta_variation*k_angle, percent);
       } 
       else {
         EastMotor.stop();
@@ -191,7 +203,7 @@ void usercontrol(void) {
     // end added part
 
     // The gyroscope should always keep in consideration the orientation of the robot, and change the input by the degrees rotated
-    double delta = original_heading - Gyroscope.rotation(degrees);
+    delta = original_heading - Gyroscope.rotation(degrees);
     if (delta > 180) delta -= 360;
     if (delta < -180) delta += 360;
     double delta_rad = delta * PI / 180;
@@ -202,8 +214,15 @@ void usercontrol(void) {
     
     Brain.Screen.clearLine(1);
     //Brain.Screen.printAt(10, 20, "Δ: %.2f°", delta);
-    Brain.Screen.printAt(10, 30, "light sensed: %.2f", light_sensed);
+    //Brain.Screen.printAt(10, 30, "light sensed: %.2f", light_sensed);
     screenCounter = 0;
+
+    double Distance_to_x = Distance_x.objectDistance(inches);
+    double Distance_to_y = Distance_y.objectDistance(inches);
+    Brain.Screen.clearLine(1);
+    Brain.Screen.clearLine(2);
+    Brain.Screen.printAt(10, 30, "Distance_to_x: %.2f", Distance_to_x);
+    //Brain.Screen.printAt(10, 60, "Distance_to_y: %.2f", Distance_to_y);
 
     
     y = Controller1.Axis3.position();
